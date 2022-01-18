392ce63ee3fb0501b97c435e769e43c6d7b99274
Austin Garrison <garrisonaustin95@gmail.com>
2022-01-18 00:05:00 -0500

    Multiple updates

--- Assets/Scripts/ThrownObject.cs
+++ Assets/Scripts/ThrownObject.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class ThrownObject : MonoBehaviour
 {
     public GameObject fractured;
+<<<<<<< Updated upstream
 
     public void BreathTheThing()
     {
         Instantiate(fractured, transform.position, transform.rotation);
+=======
+    public GameObject hidden;
+
+    public void BreakTheThing()
+    {
+        Instantiate(fractured, transform.position, transform.rotation);
+        Instantiate(hidden, transform.position, transform.rotation);
+>>>>>>> Stashed changes
         Destroy(gameObject);
     }
 }
